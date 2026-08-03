4ece3606a28cfebc53057ff85a48668193a4d9b6
Jinny Kim <60960835+jiinkim-maker@users.noreply.github.com>
2026-08-03 23:42:20 +0900

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import App from "../../../portfolio-v3-image-path-fix/portfolio-v3/src/App";
+import App from "./App";
 import { LanguageProvider } from "./context/LanguageProvider"; // 🛠 LanguageProvider만 import
 import "./styles/global.css";
 
